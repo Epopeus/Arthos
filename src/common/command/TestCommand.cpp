1704ef50c5af98cb4487d8f07c49f946392f6900
Toinouze <>
2019-04-27 19:14:14 +0200

    Removed unused code

--- src/common/command/TestCommand.cpp
+++
@@ -1,8 +0,0 @@
-#include "TestCommand.h"
-
-TestCommand::TestCommand(Guid& callingCharId_):GameClientCommand(callingCharId_) {
-}
-
-void TestCommand::run(int test, std::string test2, int test3) {
-
-}
