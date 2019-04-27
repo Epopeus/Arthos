c07c03890c7c2e4855bbd4e8111d1755d55b5b91
Toinouze <>
2019-04-27 15:51:08 +0200

    Remove unused code

--- src/ops/proxy/ForwardPacket.cpp
+++
@@ -1,9 +0,0 @@
-#include "ForwardPacket.h"
-
-ForwardPacket::ForwardPacket(NetworkConnectionId& clientSessionId_, Bytes& bytes_):PacketToSend(bytes_), clientSessionId(clientSessionId_) {
-
-}
-
-Bytes& ForwardPacket::toBytes() {
-    return bytes;
-}
